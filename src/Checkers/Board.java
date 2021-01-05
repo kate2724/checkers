a83c647e6015f77c2b8c015f2687162f72b6ca25
kate2724 <kbreen@macalester.edu>
2021-01-04 18:54:51 -0600

    Created board

---
+++ src/Checkers/Board.java
@@ -0,0 +1,7 @@
+package Checkers;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel{
+    public Board() {}
+}
--- src/Checkers/Checkers.java
+++ src/Checkers/Checkers.java
@@ -1,8 +1,30 @@
 package Checkers;
 
-public class Checkers {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Checkers extends JFrame{
+    public Checkers() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(250, 200);
+
+        setTitle("Checkers");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("final test");
+        EventQueue.invokeLater(() -> {
+            Checkers ex = new Checkers();
+            ex.setVisible(true);
+        });
     }
 }
+
