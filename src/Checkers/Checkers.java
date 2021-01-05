19061fe8b0506ab03f895a60687e90e16bf903c4
kate2724 <kbreen@macalester.edu>
2021-01-04 18:26:07 -0600

    final test

--- src/Checkers/Checkers.java
+++ src/Checkers/Checkers.java
@@ -3,6 +3,6 @@ package Checkers;
 public class Checkers {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello world");
+        System.out.println("final test");
     }
 }
